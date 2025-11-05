df3ae17cb4b2176ff9fa0f9787bb55f62a324971
Rahul Shukla <shukla2009@gmail.com>
2025-11-05 14:01:52 +0800

    initial commit

---
+++ src/main/java/com/shukla2009/maven-pg/App.java
@@ -0,0 +1,13 @@
+package com.shukla2009.maven-pg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
